39668b74923745fe7ef35ee980c167a7ca0113af
Zach <itsaboy3366@gmail.com>
2024-01-03 21:33:19 -0500

    project setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,24 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import App from './App.jsx'
 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <SearchBooks />
+      }, {
+        path: '/saved',
+        element: <SavedBooks />
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App/>
 )
