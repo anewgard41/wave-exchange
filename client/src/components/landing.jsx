0cf8a638da8a577f6c77807080d959669b97cbbc
Neal Chris <tmbarnhart1@gmail.com>
2024-01-05 15:53:11 -0500

    Working on a new search bar

--- client/src/components/landing.jsx
+++ client/src/components/landing.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import { AudioOutlined } from '@ant-design/icons';
 import { Input, Space, Avatar } from 'antd';
 const { Search } = Input;
+
+const suffix = (
+  <AudioOutlined
+    style={{
+      fontSize: 16,
+      color: '#1677ff',
+    }}
+  />
+);
+
+const onSearch = (value, _e, info) => console.log(info?.source, value);
 
 const LandingPage = () => (
   // Vertical space to center content and take up full height of the viewport
   <Space direction="vertical" align="center" style={{ height: '80vh' }}>
     {/* Image component */}
     <Avatar size={600} src="../images/waveexchange_logo.png"
+    />
+    
+    <Search
+      placeholder="input search text"
+      enterButton="Search"
+      size="large"
+      suffix={suffix}
+      onSearch={onSearch}
     />
 
     {/* Search input component - loading */}
