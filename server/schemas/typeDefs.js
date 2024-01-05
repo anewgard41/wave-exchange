1c7bc5087f330e7e7899b251ae8326aaaf7bd28d
LuminiferousGiraffe <andreas.quist99@gmail.com>
2024-01-04 21:34:11 -0500

    temp commit

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,24 +1,3 @@
-const typeDefs = `
-    type User {
-        _id: ID
-        username: String
-        password: String
-        donation: Number
-        savedMusic: [Music]
-    }
-
-    type Music {
-        artists: [String]
-        songId: String
-        songTitle: String
-    }
-
-    type Query {
-        
-    }
-`;
-
-module.exports = typeDefs;
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
