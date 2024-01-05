ed171ea1fd250f5fae11064fcb1f272ecf32f795
Neal Chris <tmbarnhart1@gmail.com>
2024-01-04 20:19:31 -0500

    Signup page username and password

--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -1,5 +1,49 @@
-function SignupPage() {
-  return <h2>Test</h2>;
-}
+import React from 'react';
+import { Layout, Input, Space, Tooltip } from 'antd';
+import { InfoCircleOutlined, UserOutlined, EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+
+const { Content } = Layout;
+const layoutStyle = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#252422', // Fixed the color value
+};
+
+const SignupPage = () => {
+  const [passwordVisible, setPasswordVisible] = React.useState(false);
+
+  return (
+    <Layout style={layoutStyle}>
+      <Content style={layoutStyle}>
+        {/* Username input */}
+        <Input
+          placeholder="Enter your username"
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          suffix={
+            <Tooltip title="Extra information">
+              <InfoCircleOutlined
+                style={{
+                  color: 'rgba(0, 0, 0, 0.45)',
+                }}
+              />
+            </Tooltip>
+          }
+        />
+
+        {/* Password input with eye icon */}
+        <Space direction="vertical">
+          <Input.Password placeholder="Input password" />
+
+          <Input.Password
+            placeholder="Input password"
+            iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+          />
+        </Space>
+      </Content>
+    </Layout>
+  );
+};
 
 export default SignupPage;
