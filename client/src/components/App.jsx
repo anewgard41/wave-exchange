40753fddc10821e8f7d5d6c840c7965b3dd0badc
Neal Chris <tmbarnhart1@gmail.com>
2024-01-06 22:51:55 -0500

    Fixed App change

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -37,7 +37,7 @@ function App() {
   return (
     <div className="body-container">
       <ApolloProvider client={client}>
-        <UserPage />
+        <Outlet />
       </ApolloProvider>
     </div>
   );
