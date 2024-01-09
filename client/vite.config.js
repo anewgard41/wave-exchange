f56e12b5e1cf70b3d352e267d6948e1a4e1fa9bd
Collier Cobb <collierj.cobb@gmail.com>
2024-01-09 01:26:38 -0500

    test2

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+//A4dN2XAcY9UOq2y6 musiclover
 //tcJWUe4QFMTI9Wx0 71.70.172.9/32
 // https://vitejs.dev/config/
 export default defineConfig({
