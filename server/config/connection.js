968f0da6893e55a692eca22980c6d594faa7f389
Zach <itsaboy3366@gmail.com>
2024-01-09 20:19:21 -0500

    fixed button/nav shadows. added comments in connection.js for easy access to both prod and test db endpoints

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/musicdb');
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/musicdb");
+
+// Prod DB
+// process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/musicdb"
+
+// Test DB
+// "mongodb+srv://musiclover:A4dN2XAcY9UOq2y6@cluster0.ll9kvjn.mongodb.net/?retryWrites=true&w=majority"
+
+// Remove above comments at final deployment
 
 module.exports = mongoose.connection;
