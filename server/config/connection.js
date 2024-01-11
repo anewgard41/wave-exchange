6a973543d476c70109694c44cd0d24a513815e2d
LuminiferousGiraffe <andreas.quist99@gmail.com>
2024-01-10 21:37:39 -0500

    temp commit

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://musiclover:A4dN2XAcY9UOq2y6@cluster0.ll9kvjn.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/musicdb");
 // mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://musiclover:A4dN2XAcY9UOq2y6@cluster0.ll9kvjn.mongodb.net/?retryWrites=true&w=majority");
 
 // Prod DB
