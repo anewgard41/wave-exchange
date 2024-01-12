5b13f60017257ca8c05ce4e32b21f052886f90cd
colcob98 <collierj.cobb@gmail.com>
2024-01-11 20:58:54 -0500

    test

--- server/server.js
+++ server/server.js
@@ -170,6 +170,10 @@ const startServer = async () => {
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")));
+
+    app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
   }
 
   const PORT = process.env.PORT || 3000;
